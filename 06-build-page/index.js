53648ca53f7f42da66c4ee847f232633d06a8a56
ForsyteRun <forsyterun@gmail.com>
2023-05-06 20:28:25 +0300

    refactor: remove dir

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const {mkdir, readdir, copyFile } = require('node:fs/promises');
+const { readdir, copyFile } = require('node:fs/promises');
 
 (() => {
   
-  fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, () => {})
-  const rs = fs.createReadStream(path.join(__dirname, "./template.html"));
+  fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, () => {});
+  const rs = fs.createReadStream(path.join(__dirname, './template.html'));
   const ws = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
   
   let str = '';
@@ -14,75 +14,75 @@ const {mkdir, readdir, copyFile } = require('node:fs/promises');
     str = data.toString();
 
     const createTemplate = (fileName) => {
-      return `{{${fileName}}}`
-    }
+      return `{{${fileName}}}`;
+    };
 
-    fs.readdir(path.join(__dirname, "./components"), {withFileTypes: true}, (error, data) => {
+    fs.readdir(path.join(__dirname, './components'), {withFileTypes: true}, (error, data) => {
       const templates = [];
       data.forEach(el => {
         const fileName = el.name.match(/([\w]*\.)*/)[0].replace('.', '');
         // const fileBody = path.parse(el.name)
         // console.log(fileName,fileBody.name);
         templates.push(createTemplate(fileName));
-      })
+      });
 
-      readdir(path.join(__dirname, "./components"), () => {}).then((n) => {
+      readdir(path.join(__dirname, './components'), () => {}).then((n) => {
         n.forEach((comp, index) => {
-          const readStream = fs.createReadStream(path.join(__dirname, "./components", comp), "utf-8");
-          readStream.on("data", (data) => {
-            str = str.replace(templates[index], data)
+          const readStream = fs.createReadStream(path.join(__dirname, './components', comp), 'utf-8');
+          readStream.on('data', (data) => {
+            str = str.replace(templates[index], data);
 
             if (!templates.find(temp => str.includes(temp))) {
-              ws.write(str)
+              ws.write(str);
             }
-          })
-        })
-      })
+          });
+        });
+      });
     });
-  })
-})()
+  });
+})();
 
 
 const mergeStyles = async () => {
   const pathStylesDir = path.join(__dirname, 'styles');
   const pathBundle = path.join(__dirname, 'project-dist', 'style.css');
-  const writeStream = fs.createWriteStream(pathBundle)
+  const writeStream = fs.createWriteStream(pathBundle);
 
   const readStyles = await readdir(pathStylesDir, () => {});
   for (const i of readStyles) {
     const ext = path.extname(i);
     if (ext === '.css') {
       const pathStylesDir = path.join(__dirname, 'styles', i);
-     const readStream = fs.createReadStream(pathStylesDir);
-     readStream.pipe(writeStream)
-   }
+      const readStream = fs.createReadStream(pathStylesDir);
+      readStream.pipe(writeStream);
+    }
   }
 };
 
-mergeStyles()
+mergeStyles();
 
 const copyDir = () => {
- fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), {recursive: true}, () => {})
+  fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), {recursive: true}, () => {});
 
- async function dirChain(fromDir, toDir) {
-  await readdir(fromDir, {withFileTypes: true})
-   .then(files => {
-    console.log(files);
-     files.forEach( async (file) => {
-       if (file.isDirectory()) {
-         const pathFromDir = path.join(fromDir, file.name);
-         const pathToDir = path.join(toDir, file.name);
-         dirChain(pathFromDir, pathToDir);
-        } else {
-          fs.mkdir(toDir, {recursive: true}, error => {
-            console.log(error);
-          });
-          copyFile(path.join(fromDir, file.name), path.join(toDir, file.name))
-        }
-      })
-    });
+  async function dirChain(fromDir, toDir) {
+    await readdir(fromDir, {withFileTypes: true})
+      .then(files => {
+        console.log(files);
+        files.forEach( async (file) => {
+          if (file.isDirectory()) {
+            const pathFromDir = path.join(fromDir, file.name);
+            const pathToDir = path.join(toDir, file.name);
+            dirChain(pathFromDir, pathToDir);
+          } else {
+            fs.mkdir(toDir, {recursive: true}, error => {
+              console.log(error);
+            });
+            copyFile(path.join(fromDir, file.name), path.join(toDir, file.name));
+          }
+        });
+      });
   }
-  dirChain(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'))
-}
+  dirChain(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
+};
 
 copyDir();
